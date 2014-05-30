749971cd19873bec77522dd5136f34d8ca694cb8
Ian Serlin <ian@ianserlin.com>
2014-05-30 01:29:07 -0700

    removing bootstrap-3 package dependency

--- package.js
+++ package.js
@@ -3,6 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use(['bootstrap-3', 'jquery'], 'client');
+    api.use(['jquery'], 'client');
     api.add_files('lib/bootbox.js', "client");
 });
